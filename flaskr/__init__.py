634aa215e8797928a69b545573c96c004552897e
Victor Joseph <victornjoseph@gmail.com>
2020-06-27 01:14:13 +0300

    basic crud methods for blog blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -20,16 +20,18 @@ def create_app(test_config = None):
 	except OSError:
 		pass
 
-	@app.route("/")
-	def hello():
-		return 'Hello, World!'
+	# @app.route("/")
+	# def hello():
+	# 	return 'Hello, World!'
 
 	from . import db
 	db.init_app(app)
 
-	# Register auth Blueprint
+	# Register app Blueprints
 	from . import auth
+	from . import blog
 	app.register_blueprint(auth.bp)
-
+	app.register_blueprint(blog.bp)
+	app.add_url_rule('/', endpoint = 'index')
 	return app
 
